8ee1fb85d9b6ba0cbfeb8017dcc3f7126e9822ec
Joshua Saikali <91435428+JoshuaSaikali@users.noreply.github.com>
2023-06-11 23:25:40 -0400

    Delete gpt-summary-extension directory

--- gpt-summary-extension/js/content.js
+++
@@ -1,5 +0,0 @@
-// Send a message to the background script with the loaded content
-chrome.runtime.sendMessage(
-	{ action: "contentLoaded", content: document.body.innerText },
-	function (response) {}
-);
